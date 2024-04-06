862f30f60d5bdd68598229bdfeb84d75be9f0088
AdamPach <adam.pach@outlook.cz>
2024-04-06 19:35:47 +0200

    Use value selected currency for counting a exchange rate

--- CnbConverter/src/components/CurrenciesCard.tsx
+++ CnbConverter/src/components/CurrenciesCard.tsx
@@ -1,10 +1,32 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect, useRef} from "react";
 import {IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonInput} from "@ionic/react";
 import {CurrenciesContext} from "../contexts/CurrenciesContext";
 
 const CurrenciesCard: React.FC = ()  => {
 
+    const inputCurrency = useRef<HTMLIonInputElement>(null);
+    const outputCurrency = useRef<HTMLIonInputElement>(null);
+
     const {selectedCurrency} = useContext(CurrenciesContext);
+
+    useEffect(() => {
+        inputCurrency.current!.value = "";
+        outputCurrency.current!.value = "";
+    }, [selectedCurrency]);
+
+    const onInputChange = (event: CustomEvent) => {
+
+        const value: number = Number(event.detail.value);
+
+        if(selectedCurrency !== null)
+        {
+
+            console.log(selectedCurrency);
+
+            outputCurrency.current!.value = value / selectedCurrency.rate * selectedCurrency.unit;
+        }
+
+    }
 
     return (
         <IonCard>
@@ -14,13 +36,16 @@ const CurrenciesCard: React.FC = ()  => {
 
             <IonCardContent>
                 <IonInput
+                    ref={inputCurrency}
                     fill="outline"
                     type={"number"}
-                    label={"CZK"}/>
+                    label={"CZK"}
+                    onIonInput={onInputChange}/>
 
                 <br/>
 
                 <IonInput
+                    ref={outputCurrency}
                     fill="outline"
                     type={"number"}
                     disabled={true}
