dc9473f950c6a475c14a3a40d2e959292475fd22
AdamPach <adam.pach@outlook.cz>
2024-04-14 09:47:54 +0200

    Add a fetching mechanism for the graph to the app

--- CnbConverter/src/components/GraphView.tsx
+++ CnbConverter/src/components/GraphView.tsx
@@ -14,45 +14,76 @@ const GraphView: React.FC<GraphViewProps> = ({selectedCurrency}) => {
 
     const canvasRef = useRef<HTMLCanvasElement>(null);
 
+    const fetchGraphData = async (): Promise<number[]> => {
+        const currDate = new Date();
+        const data: number[] = [];
+
+        for(let i = 0; i < 10; i++)
+        {
+            const date = new Date();
+            date.setDate(currDate.getDate() - 30 * i);
+
+            const response = await fetch(`http://linedu.vsb.cz/~mor03/TAMZ/cnb_json.php?lang=en&date=${date.toISOString().split('T')[0]}`);
+
+            if(response.ok)
+            {
+                const responseJson = await response.json();
+
+                const currency = responseJson.data.find((currency: Currency) => currency.code === selectedCurrency.code);
+
+                if(currency !== undefined)
+                {
+                    data.push(currency.rate * currency.unit);
+                }
+            }
+        }
+
+        data.reverse();
+        return data;
+    }
+
     useEffect(() => {
         if(canvasRef.current !== null)
         {
-            const points: number[] = [22.69,23.13,23.0,23.84,24.5, 24.17];
 
-            const pointMin = Math.floor(Math.min(...points));
-            const pointMax = Math.ceil(Math.max(...points));
+            fetchGraphData()
+                .then(points => {
 
-            const canvas = canvasRef.current;
-            const ctx = canvas.getContext('2d');
+                    const pointMin = Math.floor(Math.min(...points));
+                    const pointMax = Math.ceil(Math.max(...points));
 
-            if(ctx !== null)
-            {
+                    const canvas = canvasRef.current;
+                    const ctx = canvas?.getContext('2d');
 
-                ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-
-                points.forEach((point, index) => {
-                    const x = 10 + (index * ((canvasWidth - 20) / (points.length - 1)));
-                    const y = 300 - ((point - pointMin)*(canvasHeight / (pointMax - pointMin)));
-
-                    if(index > 0)
+                    if(ctx !== null)
                     {
-                        ctx.lineTo(x, y);
-                        ctx.stroke();
-                        ctx.closePath();
+
+                        ctx!.clearRect(0, 0, canvasWidth, canvasHeight);
+
+                        points.forEach((point, index) => {
+                            const x = 10 + (index * ((canvasWidth - 20) / (points.length - 1)));
+                            const y = 300 - ((point - pointMin)*(canvasHeight / (pointMax - pointMin)));
+
+                            if(index > 0)
+                            {
+                                ctx!.lineTo(x, y);
+                                ctx!.stroke();
+                                ctx!.closePath();
+                            }
+
+                            ctx!.beginPath();
+
+                            ctx!.arc(x, y, 5, 0, 2 * Math.PI);
+
+                            ctx!.fillText(point.toString(), x - 10, y - 10);
+
+                            ctx!.fill();
+                            ctx!.stroke();
+
+                            ctx!.moveTo(x, y);
+                        });
                     }
-
-                    ctx.beginPath();
-
-                    ctx.arc(x, y, 5, 0, 2 * Math.PI);
-
-                    ctx.fillText(point.toString(), x - 10, y - 10);
-
-                    ctx.fill();
-                    ctx.stroke();
-
-                    ctx.moveTo(x, y);
                 });
-            }
         }
     }, [selectedCurrency]);
 
